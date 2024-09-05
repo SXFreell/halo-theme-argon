5de13e1f90313a3f76db562ddcaae038f5e263c8
Freell <xfreell@163.com>
2024-09-05 23:04:49 +0800

    更新vite打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
+import { globSync } from "glob";
 import path from "path";
+
+const entryFiles = globSync(["src/main.ts", "src/libs/**/*.*", "src/page/**/*.*"]).map((file) => [
+  path.relative("src", file.slice(0, file.length - path.extname(file).length)),
+  fileURLToPath(new URL(file, import.meta.url)),
+]);
+
+console.log(Object.fromEntries(entryFiles));
 
 export default defineConfig({
   plugins: [],
   build: {
     outDir: fileURLToPath(new URL("./templates/assets/dist", import.meta.url)),
     emptyOutDir: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/main.ts"),
-      name: "main",
-      fileName: "main",
-      formats: ["iife"],
+    rollupOptions: {
+      input: Object.fromEntries(entryFiles),
+      output: {
+        format: "es",
+        entryFileNames: "[name].min.js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "css/[name].min.[ext]";
+          }
+          return "[name].min.[ext]";
+        },
+      },
     },
   },
 });
